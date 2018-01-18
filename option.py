317bc609620d68b073743d0f5e8d2601a4c9c617
Mehdi <pmzms@Mehdi.local>
2018-01-18 15:11:10 +0000

    Delta written

--- option.py
+++ option.py
@@ -5,37 +5,38 @@ from math import *
 import random
 from scipy import special
 import copy
-##
-
     
     
-class asset:
-    """asset with a given price and volatility"""
-    def __init__(self,price,vol):
-        self.price=price
-        self.vol=vol
-    def __str__(self):
-        return "current asset price=%s and volatility=%s"% (self.price,self.vol)
+#class asset:
+#    """asset with a given price and volatility"""
+#    def __init__(self,price,vol):
+#        self.price=price
+#        self.vol=vol
+#    def __str__(self):
+#        return "current asset price=%s and volatility=%s"% (self.price,self.vol)
 
 
 class option:
-    """option of a given stock with strick price K, maturity time T and interest rate r"""
-    def __init__(self,stock,K,T,r):
-        self.stock=stock
+    """option of a given asset with price s, volatility vol, strick price K, maturity time T and interest rate r"""
+    def __init__(self,s,vol,K,T,r):
+        self.asset=s
+        self.vol=vol
         self.strickp=K
         self.strickt=T
         self.interest=r
+        
     def __str__(self):
-        return "Option of an asset [%s] with strick price %s, maturity time %s year(s) and risk-free interest rate %s per year"%(self.stock,self.strickp,self.strickt,self.interest)
+        return "Option of an asset with price %s, vol %s, strick price %s, maturity time %s year(s) and risk-free interest rate %s per year"%(self.asset,self.vol,self.strickp,self.strickt,self.interest)
+    
     def EU_analytic(self,ty):
         """price of European options using Black-Scholes-Merton analytic
         formula. ty (can be a character or a list of characters) determines the option type.
         'c' for call and 'p' for put.
         if ty is one character, it returns the option price. if ty is a string,
         it returns a list of option prices."""
-        s=self.stock.price
+        s=self.asset
         k=self.strickp
-        sigma=self.stock.vol
+        sigma=self.vol
         t=self.strickt
         r=self.interest
         d1=(log(s/k)+(r+sigma**2/2)*t)/(sigma*sqrt(t))
@@ -62,29 +63,31 @@ class option:
         the length of each simulated chain.
         if ty is a character, it returns (option price,Delta).
         if ty is a string, it returns a list of (option price,Delta)"""
+        #Q: I implemented Delta and pricing together. It seems to me doing them
+        #separate is not technically correct. 
         c=[]
         p=[]
         c_eps=[]
         p_eps=[]
-        s=self.stock.price
+        s=self.asset
         s_eps=s+eps
         k=self.strickp
-        sigma=self.stock.vol
+        sigma=self.vol
         t=self.strickt
         r=self.interest
         dt=t/lchain
         for j in range(sample):
-            ss=s
-            ss_eps=s_eps
+            s_t=s
+            s_t_eps=s_eps
             for i in range(lchain):
                 dz=random.gauss(0.0,1.0)
                 coeff=r*dt+sigma*sqrt(dt)*dz
-                ss=ss*(1+coeff)
-                ss_eps=ss_eps*(1+coeff)
-            c.append(exp(-r*t)*max(ss-k,0))
-            p.append(exp(-r*t)*max(k-ss,0))
-            c_eps.append(exp(-r*t)*max(ss_eps-k,0))
-            p_eps.append(exp(-r*t)*max(k-ss_eps,0))
+                s_t=s_t*(1+coeff)
+                s_t_eps=s_t_eps*(1+coeff)
+            c.append(exp(-r*t)*max(s_t - k,0))
+            p.append(exp(-r*t)*max(k - s_t,0))
+            c_eps.append(exp(-r*t)*max(s_t_eps - k,0))
+            p_eps.append(exp(-r*t)*max(k - s_t_eps,0))
         c_price=sum(c)/sample
         p_price=sum(p)/sample
         c_eps_price=sum(c_eps)/sample
@@ -101,52 +104,57 @@ class option:
             return out
         else:
             return out[0]
+        
     def binomial(self,ty,lchain=10):
         """European call option pricing using binomial method. lchain determines
         the length of the binomial tree.
         ty determines the option type. if ty is a character, it returns the option price.
         if ty is a string, it returns a list of option prices."""
-        if len(ty)>1:
+        if len(ty)>1: #to break down ty into its characters
             out=[]
             for char in ty:
                 out.append(self.binomial(char,lchain))
             return out
         if not (ty in "cp"):
             return None
-        if lchain==0 and ty=="c":            
-            return max(self.stock.price-self.strickp,0)
-        if lchain==0 and ty=="p":
-            return max(self.strickp-self.stock.price,0)
+        if lchain==0 and ty=="c": #value at the final node
+            return max(self.asset - self.strickp,0)
+        if lchain==0 and ty=="p": #value at the final node
+            return max(self.strickp - self.asset,0)
         dt=self.strickt/lchain
-        a=exp(self.interest*dt)
-        u=exp(self.stock.vol*sqrt(dt))
-        d=exp(-self.stock.vol*sqrt(dt))
+        a=exp(self.interest*dt) #Q: these numbers do not change.
+        #Is there a way to carry them out and not calculate them every time?
+        u=exp(self.vol*sqrt(dt))
+        d=exp(-self.vol*sqrt(dt))
         p=(a-d)/(u-d)
-        tempopu=copy.deepcopy(self)
-        tempopd=copy.deepcopy(self)
-        tempopu.strickt-=dt
-        tempopd.strickt-=dt
-        tempopu.stock.price=u*self.stock.price
-        tempopd.stock.price=d*self.stock.price
-        return (p*tempopu.binomial(ty,lchain-1)+(1-p)*tempopd.binomial(ty,lchain-1))/a
+        op_up=copy.deepcopy(self) #representing the option in up node
+        op_down=copy.deepcopy(self) #representing the option in down node
+        op_up.strickt -= dt
+        op_down.strickt -= dt
+        op_up.asset=u*self.asset
+        op_down.asset=d*self.asset
+        return (p*op_up.binomial(ty,lchain-1)+(1-p)*op_down.binomial(ty,lchain-1))/a
 
-
-
-    def Delta_EUput_analytic(self,eps=10.0**-3):
+    def Delta_EU_analytic(self,ty,eps=10.0**-3):
+        """Calculates the Delta of European option using EU_analytic pricer.
+        ty determines the option type."""
         temp_op=copy.deepcopy(self)
-        temp_op.stock.price+=eps
-        return (temp_op.EUput_analytic() - self.EUput_analytic())/eps
+        temp_op.asset += eps
+        out=[]
+        for char in ty:
+            if char in "cp":
+                out.append((temp_op.EU_analytic(char) - self.EU_analytic(char))/eps)
+            else:
+                out.append(None)
+        if len(ty)>1:
+            return out
+        else:
+            return out[0]
 
-    def Delta_EUcall_analytic(self,eps=10.0**-3):
-        temp_op=copy.deepcopy(self)
-        temp_op.stock.price+=eps
-        return (temp_op.EUcall_analytic() - self.EUcall_analytic())/eps    
     
-google=asset(100.0,0.3)
-op=option(google,100.0,1.0,0.1)
-MC=op.EU_MC("p")
+op=option(100.0,0.3,100.0,1.0,0.1)
 
-print op.EU_analytic("c")
-print MC
-print op.binomial("pcd")
-
+print op.EU_analytic("cpd")
+print op.EU_MC("cpd")
+print op.binomial("cpd")
+print op.Delta_EU_analytic("cpd")
